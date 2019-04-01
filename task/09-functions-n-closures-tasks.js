eac00a58aef3e3e66811d81bf42863ffe01e6a0e
RowanMorrison <47691267+RowanMorrison@users.noreply.github.com>
2019-04-01 06:31:31 +0300

    lab3

---
+++ task/09-functions-n-closures-tasks.js
@@ -0,0 +1,247 @@
+'use strict';
+
+/**********************************************************************************************
+ *                                                                                            *
+ * Plese read the following tutorial before implementing tasks:                               *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions                    *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function  *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments      *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures                           *
+ *                                                                                            *
+ **********************************************************************************************/
+
+
+/**
+ * Returns the functions composition of two specified functions f(x) and g(x).
+ * The result of compose is to be a function of one argument, (lets call the argument x),
+ * which works like applying function f to the result of applying function g to x, i.e.
+ *  getComposition(f,g)(x) = f(g(x))
+ *
+ * @param {Function} f
+ * @param {Function} g
+ * @return {Function}
+ *
+ * @example
+ *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.acos(x))
+ *
+ */
+function getComposition(f,g) {
+	//return x => f(g(x));
+	
+	return function(y) {
+		return f(g(y));
+	}
+}
+
+
+/**
+ * Returns the math power function with the specified exponent
+ *
+ * @param {number} exponent
+ * @return {Function}
+ *
+ * @example
+ *   var power2 = getPowerFunction(2); // => x^2
+ *   power2(2) => 4
+ *   power2(4) => 16
+ *
+ *   var power05 = getPowerFunction(0.5); // => x^0.5
+ *   power05(4) => 2
+ *   power05(16) => 4
+ *
+ */
+function getPowerFunction(exponent) {
+	//return x => Math.pow(x, exponent);
+	
+	return function(num) {
+		return Math.pow(num, exponent);
+	}
+}
+
+
+/**
+ * Returns the polynom function of one argument based on specified coefficients.
+ * See: https://en.wikipedia.org/wiki/Polynomial#Definition
+ *
+ * @params {integer}
+ * @return {Function}
+ *
+ * @example
+ *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
+ *   getPolynom(1,-3)  => y = x - 3
+ *   getPolynom(8)     => y = 8
+ *   getPolynom()      => null
+ */
+ 
+ /*The inner variables of the inner functions act as safe stores for the outer arguments and variables. 
+ They hold "persistent" and "encapsulated" data for the inner functions to work with. 
+ The functions do not even have to be assigned to a variable, or have a name.*/
+ 
+function getPolynom() {
+	if (arguments.length == 0) return null;
+	let toCount = [];
+	let i = arguments.length - 1;
+	while (i >= 0) {
+		toCount.push({exp: arguments.length - 1 - i, coef: arguments[i]});
+		i--;
+	}
+	function insidePolynome(x) {
+		let result = 0;
+		toCount.map(function(val) {
+			result += val.coef * Math.pow(x, val.exp);
+		});
+		return result;
+	}
+	return insidePolynome;
+}
+
+
+/**
+ * Memoizes passed function and returns function
+ * which when invoked for the first time calls the passed function and then always returns cached result.
+ *
+ * @params {Function} func - function to memoize
+ * @return {Function} memoized function
+ *
+ * @example
+ *   var memoizer = memoize(() => Math.random());
+ *   memoizer() => some random number  (first run, evaluates the result of Math.random())
+ *   memoizer() => the same random number  (second run, returns the previous cached result)
+ *   ...
+ *   memoizer() => the same random number  (next run, returns the previous cached result)
+ */
+function memoize(func) {
+	let invoked = false;
+	let cache;
+	function inside(x){
+		if (!invoked) {
+			invoked = true;
+			cache = func(x);
+			return cache;
+		}
+		return cache;
+	}
+	return inside;
+}
+
+
+/**
+ * Returns the function trying to call a passed function and if it throws an exception,
+ * this function will try to recall a passed function specified number of times.
+ *
+ * @param {Function} func
+ * @param {number} attempts
+ * @return {Function}
+ *
+ * @example
+ * var attempt = 0, retryer = retry(() => {
+ *      if (++attempt % 2) throw new Error('test');
+ *      else return attempt;
+ * }, 2);
+ * retryer() => 2
+ */
+function retry(func, attempts) {
+	let i;
+	function inside() {
+		while (attempts != 0) {
+			try {
+				i = func();
+			}
+			catch(err) {
+				attempts--;
+				continue;
+			}
+			return i;
+		}
+	}
+	return inside;
+}
+
+
+/**
+ * Returns the logging wrapper for the specified method,
+ * Logger has to log the start and end of calling the specified function.
+ * Logger has to log the arguments of invoked function.
+ * The fromat of output log is:
+ * <function name>(<arg1>, <arg2>,...,<argN>) starts
+ * <function name>(<arg1>, <arg2>,...,<argN>) ends
+ *
+ *
+ * @param {Function} func
+ * @param {Function} logFunc - function to output log with single string argument
+ * @return {Function}
+ *
+ * @example
+ *
+ * var cosLogger = logger(Math.cos, console.log);
+ * var result = cosLogger(Math.PI));     // -1
+ *
+ * log from console.log:
+ * cos(3.141592653589793) starts
+ * cos(3.141592653589793) ends
+ *
+ */
+function logger(func, logFunc) {
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * Return the function with partial applied arguments
+ *
+ * @param {Function} fn
+ * @return {Function}
+ *
+ * @example
+ *   var fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
+ *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
+ */
+function partialUsingArguments(fn, ...arg1) {
+	//throw new Error('Not implemented');
+	function inner(...arg2){
+		function innerMost() {
+			return fn(...arg1, ...arg2);
+		}
+		return innerMost();
+	}
+	return inner;
+}
+
+
+/**
+ * Returns the id generator function that returns next integer starting from specified number every time when invoking.
+ *
+ * @param {Number} startFrom
+ * @return {Function}
+ *
+ * @example
+ *   var getId4 = getIdGenerator(4);
+ *   var getId10 = gerIdGenerator(10);
+ *   getId4() => 4
+ *   getId10() => 10
+ *   getId4() => 5
+ *   getId4() => 6
+ *   getId4() => 7
+ *   getId10() => 11
+ */
+function getIdGeneratorFunction(startFrom) {
+	function innerMost() {
+		return startFrom++;
+	}
+	return innerMost;
+}
+
+
+module.exports = {
+    getComposition: getComposition,
+    getPowerFunction: getPowerFunction,
+    getPolynom: getPolynom,
+    memoize: memoize,
+    retry: retry,
+    logger: logger,
+    partialUsingArguments: partialUsingArguments,
+    getIdGeneratorFunction: getIdGeneratorFunction,
+};
