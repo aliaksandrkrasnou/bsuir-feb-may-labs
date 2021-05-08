291323aa1c59f3a31ddbcc14730b971cdd3df8ce
Pavel Kastsiukou <p.kastsiukou@roboforex.com>
2021-05-08 09:33:29 +0300

    Lab 4

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const Merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CommonConfig = require('./webpack.common.js');
+
+module.exports = Merge(CommonConfig, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    }),
+  ],
+});
